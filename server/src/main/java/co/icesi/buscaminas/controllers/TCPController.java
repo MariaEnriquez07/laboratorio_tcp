6660446bfeb10c566ac2513e9777453278390175
Maria Camila Cordoba <cordobamariacamila03@gmail.com>
2026-09-22 21:24:13 -0500

    Cambios en TCTController

--- server/src/main/java/co/icesi/buscaminas/controllers/TCPController.java
+++ server/src/main/java/co/icesi/buscaminas/controllers/TCPController.java
@@ -132,7 +132,19 @@ public class TCPController {
                         response.status = "OK";
                         response.data.put("board", board);
                         break;
-
+                    case "MARK_CELL":
+                        int mi = Integer.parseInt(data.get("i"));
+                        int mj = Integer.parseInt(data.get("j"));
+                        try {
+                            services.markCell(mi, mj);
+                            response.status = "OK";
+                            board = services.printBoard();
+                            response.data.put("board", board);
+                        } catch (Exception e) {
+                            response.status = "ERROR";
+                            response.data.put("message", e.getMessage());
+                        }
+                        break;
                     default:
                         break;
                 }
@@ -151,6 +163,11 @@ public class TCPController {
             }
         }
 
+    }
+
+    public Cell[][] markCell(int i, int j) {
+        game.markCell(i, j);
+        return game.getBoard();
     }
 
 }
